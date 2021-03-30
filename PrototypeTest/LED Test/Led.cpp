438e16263ecce77f951b3931de9e2079330015e2
zeyuan-song0204 <77240402+zeyuan-song0204@users.noreply.github.com>
2021-03-30 14:35:00 +0100

    Create Led.cpp

---
+++ PrototypeTest/LED Test/Led.cpp
@@ -0,0 +1,45 @@
+#include<wiringPi.h>
+#include<stdio.h>
+#define RED 25
+#define YELLOW  24
+#define GREEN 23
+int main(void){
+    int t;
+    if(wiringPiSetup()==-1){//初始化
+        printf("setup wiringPi failed!");
+        return 1;
+    }
+    pinMode(RED,OUTPUT);
+    pinMode(YELLOW,OUTPUT);
+    pinMode(GREEN,OUTPUT);
+    while(1){
+        t=1;
+        if(t==1){
+            printf("red on\n");
+            digitalWrite(RED,HIGH);
+            digitalWrite(YELLOW,LOW);
+            digitalWrite(GREEN,LOW);
+            delay(2000);
+            t=2;
+        }
+        if(t==2){
+            printf("yellow on\n");
+            digitalWrite(RED,LOW);
+            digitalWrite(YELLOW,HIGH);
+            digitalWrite(GREEN,LOW);
+            delay(2000);
+            t=3;
+        }
+        if(t==3){
+            printf("green on\n");
+            digitalWrite(RED,LOW);
+            digitalWrite(YELLOW,LOW);
+            digitalWrite(GREEN,HIGH);
+            delay(2000);
+            t=1;
+        }
+        
+    }
+    return 0;
+}
+//gcc -o led led.cpp -lwiringPi
